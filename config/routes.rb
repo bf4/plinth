59dc53af9476418c81f7baa0e7ab3439fe084c6b
Ad Taylor <ad@adtaylor.co.uk>
2012-08-24 10:51:20 +0100

    Adds in temp route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 SbStyleguide::Application.routes.draw do
+  
+  #
+  # Temp root to start project
+  #
+
+  root :to => "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

0993398a7ea24f81c062d64e24607ff9c4e24ee8
Ad Taylor <ad@adtaylor.co.uk>
2012-09-28 16:38:17 +0100

    Brutal hack of the config files

--- config/initializers/haml_coderay.rb
+++
@@ -1,5 +0,0 @@
-Haml::Filters::CodeRay.encoder_options = {
-  :line_numbers => :table,
-  :wrap => :div,
-  :break_lines => false
-}
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SbStyleguide::Application.config.secret_token = '06b5d7caf2a8297a7a19da6ac1aaa973fb36cfecdc3c635d9e2b738af629de5ae28fffb50d0d7d3176ec0bc891983ef404782caab01c70fa47cf412e975dc0ea'

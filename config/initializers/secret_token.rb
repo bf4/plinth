1fe4f8871675a57d6ddd80c48b6be54f6ddb494d
Radu Busuioc <ra.busuioc@gmail.com>
2013-12-12 11:06:24 +0000

    adding secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Plinth::Application.config.secret_token = '06b5d7caf2a8297a7a19da6ac1aaa973fb36cfecdc3c635d9e2b738af629de5ae28fffb50d0d7d3176ec0bc891983ef404782caab01c70fa47cf412e975dc0ea'
+Plinth::Application.config.secret_key_base = '05f0fc60436a04bc8c8458959dc38d0f5c74c8bb7f722be2d265943b07070739791be0e163a33cf44d1e1bc2ef2b0e97318566a0c21ce9f36a65d5b33498f4b2'
